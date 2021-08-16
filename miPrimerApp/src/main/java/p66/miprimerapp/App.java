21f34ed494ebac44277c50761d90a6e575098cb0
Natacha7 <jennynatacha@misena.edu.co>
2021-08-16 16:52:18 -0500

    Proyecto tienda en NetBeans

---
+++ miPrimerApp/src/main/java/p66/miprimerapp/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p66.miprimerapp;
+
+/**
+ *
+ * @author Natacha Libreros
+ */
+public class App {
+    public static void main (String arg[]) {
+        System.out.println("Hola mundo NetBeans");
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+        miVentana.setVisible(true);
+    }
+    
+}
